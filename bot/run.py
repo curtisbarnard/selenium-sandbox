83d2ad0da346a4bdd7acbbb283f2044c0e484e56
Curtis Barnard <curtis.barnard.jr@gmail.com>
2022-10-07 12:04:47 -0700

    add exception and PATH info to run script from terminal

--- bot/run.py
+++ bot/run.py
@@ -1,10 +1,24 @@
 from booking.booking import Booking
 
-with Booking() as bot:
-    bot.land_first_page()
-    bot.select_place_to_go('Tokyo')
-    # below will only select dates within the two panes that get open, TODO figure out how to select future dates
-    bot.select_dates('2022-10-15', '2022-10-30')
-    bot.select_guest(2)
-    bot.search()
-    bot.apply_filters()
+try:
+    with Booking() as bot:
+        bot.land_first_page()
+        bot.select_place_to_go('Tokyo')
+        # below will only select dates within the two panes that get open, TODO figure out how to select future dates
+        bot.select_dates('2022-10-15', '2022-10-30')
+        bot.select_guest(2)
+        bot.search()
+        bot.apply_filters()
+except Exception as e:
+    if 'in PATH' in str(e):
+        print(
+            'You are trying to run the bot from the terminal \n'
+            'Please add your Selenium Drivers to the PATH variable \n'
+            'Windows Command Line: \n'
+            '   set PATH=%PATH%;C:/Users/Curtis/Documents/seleniumdrivers/ \n'
+            '   Replace the forward slashes with back slashes above \n \n'
+            'Git BASH Terminal: \n'
+            ' PATH=$PATH:/C/Users/Curtis/Documents/seleniumdrivers/'
+        )
+    else:
+        raise
